184583520b6453c9370d499f693d5ab1e18d2f1e
JoHamann <93760928+JoHamann@users.noreply.github.com>
2022-01-10 12:49:06 +0100

    added tessobj class to init file

--- tesspy/__init__.py
+++ tesspy/__init__.py
@@ -1,4 +1,6 @@
-from .tesspy import TessObj
+from .tesspy import *
 
 name = "tesspy"
 __version__ = "0.0.1"
+__author__ = "Siaveh Saki and Jonas Hamann"
+__author_email__ = "jonas.hamann@fb3.fra-uas.de"
