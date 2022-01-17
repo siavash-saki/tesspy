7a52a365efaffff50129794b22713df5c9d5e53e
siavash-saki <siavash.saki@engineer.com>
2022-01-17 15:19:23 +0100

    updated tessellation class

--- tesspy/tessellation.py
+++ tesspy/tessellation.py
@@ -5,7 +5,6 @@ import geopandas as gpd
 from geopandas import sjoin
 import osmnx as ox
 import h3
-from babelgrid import Babel
 import shapely
 from shapely.geometry import Point, Polygon, LineString, mapping, MultiPoint
 from shapely.ops import polygonize, cascaded_union
@@ -13,6 +12,9 @@ from sklearn.cluster import AgglomerativeClustering, KMeans
 from scipy.spatial import Voronoi
 from collections import defaultdict
 import mercantile
+
+from tessellation_functions import *
+from poi_data import *
 
 
 def get_city_polygon(city: str):
@@ -25,6 +27,23 @@ def get_city_polygon(city: str):
     df_city = df_city.rename({"osm_id": "osmid"})
     return df_city
 
+
+def check_input_geodataframe(gdf):
+    if len(gdf) != 1:
+        raise ValueError("GeoDataFrame must have only one polygon")
+    else:
+        if not hasattr(gdf, "geometry"):
+            raise TypeError("Geometry column missing in GeoDataFrame")
+        else:
+            if type(gdf['geometry'].iloc[0]) != Polygon:
+                raise TypeError("Geometry must be of type shapely polygon")
+            else:
+                if gdf.crs is None:
+                    raise ValueError("GeoDataFrame must have a CRS")
+                elif gdf.crs != 'epsg:4326':
+                    return gdf.to_crs(epsg=4326)
+                else:
+                    return gdf
 
 
 ## we don't need this --> input either geopandas or string
@@ -46,24 +65,20 @@ def get_city_polygon(city: str):
 #     return dataset
 
 
-
-
 class Tessellation:
 
     def __init__(self, area):
 
-        # TODO: are should be a geodataframe with a single polygone (or MultiPolygon) and available crs
-        # TODO: if crs is not epsg:4326 then convert
+        # todo: can input be MultiPolygon???
         if type(area) == gpd.GeoDataFrame:
-            self.area_gdf = area
-
+            self.area_gdf = check_input_geodataframe(area)
         elif type(area) == str:
             self.area_gdf = get_city_polygon(area)
-
         else:
             raise TypeError("area must be in format: GeoDataFrame or string")
 
         self.poi_data = None
+        self.available_poi_categories = []
         self.road_network = None
 
     def squares(self, resolution: int):
@@ -72,8 +87,8 @@ class Tessellation:
         :param city: Must be a shapely.Polygon, GeoDataFrame Polygon or String e.g. "Frankfurt am Main"
         :return:GeoDataFrame with hexagons for the given city, geometry in shapely-Polygon
         """
-        df_qk = square_polyfill(self.area_gdf, resolution)
-        return df_qk
+        df_qk_squares = get_squares_polyfill(self.area_gdf, resolution)
+        return df_qk_squares
 
     def hexagons(self, resolution: int):
         """
@@ -82,10 +97,27 @@ class Tessellation:
         :return: GeoDataFrame with hexagons for the given city, geometry in shapely-Polygon
         """
 
-        df_h3 = get_h3(self.area_gdf, resolution)
-        return df_h3
+        df_h3_hexagons = get_h3_hexagons(self.area_gdf, resolution)
+        return df_h3_hexagons
 
-    def adaptive_quadkey(self, start_resolution, poi_categories=["amenity",'building'], threshold=None):
+    def adaptive_quadkey(self,
+                         start_resolution: int,
+                         poi_categories=["amenity", 'building'],
+                         threshold=None,
+                         timeout=60):
+
+        for poi_category in poi_categories:
+            if poi_category in self.available_poi_categories:
+                poi_categories.remove(poi_category)
+            else:
+                self.available_poi_categories.append(poi_category)
+
+        if len(poi_categories)>0:
+            pass
+
+
+
+
 
 
         # tiles = Babel("bing").polyfill(df_city.geometry.unary_union, resolution=start_resolution)
@@ -105,7 +137,7 @@ class Tessellation:
 
         return aqk_count
 
-    def voronoi(self, cluster_algo="k-means"):
+    def voronoi(self, cluster_algo="k-means", timeout=60):
         """
         :param city: Must be a shapely.Polygon, GeoDataFrame Polygon or String e.g. "Frankfurt am Main"
         :param data: GeoDataFrame with a geometry column representing data like POI or generators for Voronoi Diagrams
@@ -113,6 +145,16 @@ class Tessellation:
         cluster algorithm
         :return: GeoDataFrame with Polygons representing the voronoi polygons
         """
+
+        for poi_category in poi_categories:
+            if poi_category in self.available_poi_categories:
+                poi_categories.remove(poi_category)
+            else:
+                self.available_poi_categories.append(poi_category)
+
+        if len(poi_categories) > 0:
+            pass
+
 
         data_locs = np.column_stack([data.geometry.x, data.geometry.y])
 
