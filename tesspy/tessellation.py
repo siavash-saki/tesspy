4d910b1c54402163a507f92ea352503d9f179afe
siavash-saki <siavash.saki@engineer.com>
2022-01-17 19:28:59 +0100

    Modified tessellation object

--- tesspy/tessellation.py
+++ tesspy/tessellation.py
@@ -9,9 +9,10 @@ import shapely
 from shapely.geometry import Point, Polygon, LineString, mapping, MultiPoint
 from shapely.ops import polygonize, cascaded_union
 from sklearn.cluster import AgglomerativeClustering, KMeans
-from scipy.spatial import Voronoi
 from collections import defaultdict
 import mercantile
+
+from scipy.spatial import Voronoi
 
 from tessellation_functions import *
 from poi_data import *
@@ -28,7 +29,7 @@ def get_city_polygon(city: str):
     return df_city
 
 
-def check_input_geodataframe(gdf):
+def _check_input_geodataframe(gdf):
     if len(gdf) != 1:
         raise ValueError("GeoDataFrame must have only one polygon")
     else:
@@ -69,32 +70,96 @@ class Tessellation:
 
     def __init__(self, area):
 
+        """
+        Creates a Tessellation object using a GeoDataFrame or a city name,
+        which allows for different tessellation methods
+
+        Parameters
+        ----------
+        area : GeoDataFrame or str
+            GeoDataFrame must have a single polygon in geometry column and
+            its CRS must be defined.
+            str must be a name of a city
+
+        Examples
+        --------
+        >>> ffm= Tessellation('Frankfurt am Main')
+        todo: write examples
+        """
+
         # todo: can input be MultiPolygon???
         if type(area) == gpd.GeoDataFrame:
-            self.area_gdf = check_input_geodataframe(area)
+            self.area_gdf = _check_input_geodataframe(area)
         elif type(area) == str:
             self.area_gdf = get_city_polygon(area)
         else:
             raise TypeError("area must be in format: GeoDataFrame or string")
 
-        self.poi_data = None
+        self.poi_dataframe = pd.DataFrame()
         self.available_poi_categories = []
-        self.road_network = None
+        self.road_network = pd.DataFrame()
+
+    def _get_missing_poi_categories(self, poi_list):
+        """
+        Checks it the poi categories are already available in the object poi_dataframe
+        Creates a list of missing categories, which should be downloaded from OSM
+
+        Parameters
+        ----------
+        poi_list : list
+            A list of passed POI categories
+
+        Returns
+        -------
+        missing_poi_categories : list
+            A list containing the POI categories which should be downloaded
+        """
+        missing_poi_categories = []
+        for poi_category in poi_list:
+            if not hasattr(self.poi_dataframe, poi_category):
+                missing_poi_categories.append(poi_category)
+        return missing_poi_categories
 
     def squares(self, resolution: int):
         """
-        :param resolution: resolution for mircosoft bing. Int ∈ [1,...,23]
-        :param city: Must be a shapely.Polygon, GeoDataFrame Polygon or String e.g. "Frankfurt am Main"
-        :return:GeoDataFrame with hexagons for the given city, geometry in shapely-Polygon
+        Generate square grid laying over the area
+
+        Parameters
+        ----------
+        resolution : int
+            Specifies the size of hexagons
+            A positive number between todo: complete
+
+        Returns
+        -------
+        df_qk_squares : pandas.DataFrame
+            Dataframe containing squares
+
+        Examples
+        --------
+        todo: write examples
         """
         df_qk_squares = get_squares_polyfill(self.area_gdf, resolution)
         return df_qk_squares
 
     def hexagons(self, resolution: int):
         """
-        :param city: Must be a shapely.Polygon, GeoDataFrame Polygon or String e.g. "Frankfurt am Main"
-        :param resolution: resolution for mircosoft bing. Int ∈ [0,...,15]
-        :return: GeoDataFrame with hexagons for the given city, geometry in shapely-Polygon
+        Generate hexagon grid laying over the area
+
+        Parameters
+        ----------
+        resolution : int
+            Specifies the size of hexagons
+            A positive number between todo: complete
+
+        Returns
+        -------
+        df_h3_hexagons : pandas.DataFrame
+            Dataframe containing hexagons
+
+        Examples
+        --------
+        todo: write examples
         """
 
         df_h3_hexagons = get_h3_hexagons(self.area_gdf, resolution)
@@ -104,21 +169,68 @@ class Tessellation:
                          start_resolution: int,
                          poi_categories=["amenity", 'building'],
                          threshold=None,
-                         timeout=60):
+                         timeout=60,
+                         verbose=False):
 
-        for poi_category in poi_categories:
-            if poi_category in self.available_poi_categories:
-                poi_categories.remove(poi_category)
-            else:
-                self.available_poi_categories.append(poi_category)
+        """
+        Generate adaptive squares based on the input POI data.
+        Squares are created at the start resolution. Each square is broken
+        into four smaller squares while the number of its POI exceeds the
+        threshold.
+        POI categories should be a list of OSM primary map features.
+        A complete list can be found on the OSM website:
+        https://wiki.openstreetmap.org/wiki/Map_features
 
-        if len(poi_categories)>0:
-            pass
+        Parameters
+        ----------
+        start_resolution : int
+            Specifies the size of initial squares
+            A positive number between todo: complete
+        poi_categories : A list of OSM primary map features, default=["amenity", 'building']
+            Possible values in the list: ['aerialway', 'aeroway', 'amenity', 'barrier',
+            'boundary', 'building', 'craft', 'emergency', 'geological', 'healthcare',
+            'highway', 'historic', 'landuse', 'leisure', 'man_made', 'military', 'natural',
+            'office', 'place', 'power', 'public_transport', 'railway', 'route', 'shop',
+            'sport', 'telecom', 'tourism', 'water', 'waterway']
+        threshold : int, default=None
+            Threshold for the number of POI in a single square. If square
+            has more, it is divided into four squares. If None passed, the median
+            number of POI per square in the initial level is used as threshold.
+        timeout : int, default=60
+            The TCP connection timeout for the request
+        verbose : bool, default=False
+            If True, print information while computing
 
+        Returns
+        -------
+        df_adaptive_squares : pandas.DataFrame
+            Dataframe containing adaptive squares
 
+        Examples
+        --------
+        todo: write examples
+        """
 
+        # check if the categories are already available in poi_data
+        # find the ones which are not available and should be downloaded from OSM
+        missing_poi_categories = self._get_missing_poi_categories(poi_categories)
 
+        # if there is any category that should be downloaded -->
+        # create the query and run it
+        if len(missing_poi_categories) > 0:
+            poi_data_obj = POIdata(self.area_gdf, missing_poi_categories, timeout, verbose)
+            poi_data_new = poi_data_obj.get_poi_data()
+            # concat the data with the available poi_data dataframe of the Tessellation object
+            self.poi_dataframe = pd.concat([self.poi_dataframe, poi_data_new]).fillna(False)
+            self.poi_dataframe = self.poi_dataframe.reset_index(drop=True)
 
+        # data, based on which, tessellation should be done
+        tess_data = self.poi_dataframe[self.poi_dataframe[poi_categories].sum(axis=1) > 0]
+        points_geom = tess_data[['center_longitude', 'center_latitude']] \
+            .apply(lambda p: Point(p['center_longitude'], p['center_latitude']), axis=1)
+        tess_data = gpd.GeoDataFrame(geometry=points_geom,
+                                     data=tess_data[poi_categories],
+                                     crs='EPSG:4326')
 
         # tiles = Babel("bing").polyfill(df_city.geometry.unary_union, resolution=start_resolution)
         # df_aqk = gpd.GeoDataFrame([t.to_dict() for t in tiles], geometry='shapely')
@@ -134,59 +246,114 @@ class Tessellation:
         #
         #     df_temp2 = count_poi(df_temp, poi_data)
         #     aqk_count = gpd.GeoDataFrame(df_temp2, geometry="shapely")
+        # return aqk_count
 
-        return aqk_count
+        pass
 
-    def voronoi(self, cluster_algo="k-means", timeout=60):
+    def voronoi(self,
+                cluster_algo="k-means",
+                poi_categories=["amenity", 'building'],
+                timeout=60,
+                n_polygons=100,
+                min_cluster_size=15,
+                verbose=False):
         """
-        :param city: Must be a shapely.Polygon, GeoDataFrame Polygon or String e.g. "Frankfurt am Main"
-        :param data: GeoDataFrame with a geometry column representing data like POI or generators for Voronoi Diagrams
-        :param cluster_algo: if not generators are used directly the spatial data is going to be clustered. specify the
-        cluster algorithm
-        :return: GeoDataFrame with Polygons representing the voronoi polygons
+        Generate Voronoi polygons based on the input POI data.
+        POI categories should be a list of OSM primary map features.
+        A complete list can be found on the OSM website:
+        https://wiki.openstreetmap.org/wiki/Map_features
+
+        Parameters
+        ----------
+        cluster_algo : {'k-means', 'hdbscan', None}, default='k-means'
+            Algorithm for clustering the POI data before creating Voronoi generators
+            If None passed, POI data are directly used as generators.
+        poi_categories : A list of OSM primary map features, default=["amenity", 'building']
+            Possible values in the list: ['aerialway', 'aeroway', 'amenity', 'barrier',
+            'boundary', 'building', 'craft', 'emergency', 'geological', 'healthcare',
+            'highway', 'historic', 'landuse', 'leisure', 'man_made', 'military', 'natural',
+            'office', 'place', 'power', 'public_transport', 'railway', 'route', 'shop',
+            'sport', 'telecom', 'tourism', 'water', 'waterway']
+        timeout : int, default=60
+            The TCP connection timeout for the request
+        n_polygons : int, default=100
+            Only when cluster_algo="k-means", number of polygons to be created
+            Positive number and less than the initial POI numbers
+        min_cluster_size : int, default=15
+            Only when cluster_algo="hdbscan", minimum cluster size
+            Positive number
+        verbose : bool, default=False
+            If True, print information while computing
+
+        Returns
+        -------
+        df_voronoi : pandas.DataFrame
+            Dataframe containing Voronoi polygons
+
+        Examples
+        --------
+        todo: write examples
         """
 
-        for poi_category in poi_categories:
-            if poi_category in self.available_poi_categories:
-                poi_categories.remove(poi_category)
-            else:
-                self.available_poi_categories.append(poi_category)
+        # check if the categories are already available in poi_data
+        # find the ones which are not available and should be downloaded from OSM
+        missing_poi_categories = self._get_missing_poi_categories(poi_categories)
 
-        if len(poi_categories) > 0:
-            pass
+        # if there is any category that should be downloaded -->
+        # create the query and run it
+        if len(missing_poi_categories) > 0:
+            poi_data_obj = POIdata(self.area_gdf, missing_poi_categories, timeout, verbose)
+            poi_data_new = poi_data_obj.get_poi_data()
+            # concat the data with the available poi_data dataframe of the Tessellation object
+            self.poi_dataframe = pd.concat([self.poi_dataframe, poi_data_new]).fillna(False)
+            self.poi_dataframe = self.poi_dataframe.reset_index(drop=True)
 
+        # data, based on which, tessellation should be done
+        tess_data = self.poi_dataframe[self.poi_dataframe[poi_categories].sum(axis=1) > 0]
+        data_locs = tess_data[['center_latitude', 'center_longitude']].values
 
-        data_locs = np.column_stack([data.geometry.x, data.geometry.y])
+        # points_geom = tess_data[['center_longitude', 'center_latitude']] \
+        #     .apply(lambda p: Point(p['center_longitude'], p['center_latitude']), axis=1)
+        # tess_data = gpd.GeoDataFrame(geometry=points_geom,
+        #                              data=tess_data[poi_categories],
+        #                              crs='EPSG:4326')
 
         # TODO: maybe include more cluster algorithms like DBSCAN,..
         if cluster_algo == "k-means":
             # TODO: Think about automating nb of clusters
-            clusterer = KMeans(n_clusters=500).fit(data_locs)
-            data["ClusterIDs"] = clusterer.labels_
+            clustering = KMeans(n_clusters=n_polygons).fit(data_locs)
+            generators = [np.mean(data_locs[clustering.labels_ == label], axis=0) for label in range(n_polygons)]
 
         elif cluster_algo == "hdbscan":
             # TODO: Think about automating min_cluster_size e.g. x% of the initial data shape
-            clusterer = hdbscan.HDBSCAN(min_cluster_size=15, prediction_data=True).fit(data_locs)
-            soft_clusters = hdbscan.all_points_membership_vectors(clusterer)
-            data["ClusterIDs"] = [np.argmax(x) for x in soft_clusters]
+            clustering = hdbscan.HDBSCAN(min_cluster_size=15, prediction_data=True).fit(data_locs)
+            soft_clusters = hdbscan.all_points_membership_vectors(clustering)
+            cluster_labels = [np.argmax(x) for x in soft_clusters]
+            generators = [np.mean(data_locs[cluster_labels == label], axis=0) for label in range(n_polygons)]
 
-        elif cluster_algo == "None":
-            data["ClusterIDs"] = data.geometry
+        elif cluster_algo is None:
+            if len(tess_data["ClusterIDs"]) > 5000:
+                raise ValueError("Too many generators for Voronoi diagram. Please select a clustering algorithm")
+            else:
+                generators = data_locs
 
         else:
-            raise ValueError("Please use one of the implemented clustering: k-mean, hdbscan, dbscan")
+            raise ValueError("Please use a clustering algorithm: k-means, hdbscan or None")
 
-        convex_hulls = [MultiPoint(data[data["ClusterIDs"] == ID]["geometry"].values).convex_hull for ID in
-                        data["ClusterIDs"].unique()]
-        gdf_generators = gpd.GeoDataFrame(geometry=convex_hulls, crs="EPSG:4326")
-        generators = gdf_generators["geometry"].centroid
 
-        voronoi_dia = Voronoi(np.column_stack([generators.x, generators.y]))
+        # Building convex hulls and calculating centroids seems unnecessary
+        # we can directly compute centroids with numpy mean of points in a cluster
+        # convex_hulls = [MultiPoint(data[data["ClusterIDs"] == ID]["geometry"].values).convex_hull for ID in
+        #                 data["ClusterIDs"].unique()]
+        # gdf_generators = gpd.GeoDataFrame(geometry=convex_hulls, crs="EPSG:4326")
+        # generators = gdf_generators["geometry"].centroid
+
+        voronoi_dia = Voronoi(generators)
         voronoi_poly = gpd.GeoDataFrame(geometry=[p for p in voronoi_polygons(voronoi_dia, 0.1)], crs="EPSG:4326")
-        vor_polygons = voronoi_poly.intersection(df_city.geometry.iloc[0])
+        vor_polygons = voronoi_poly.intersection(self.area_gdf.geometry.iloc[0])
+        df_voronoi = gpd.GeoDataFrame(geometry=vor_polygons)
 
-        df_vor = gpd.GeoDataFrame(geometry=vor_polygons)
-        return df_vor
+        return df_voronoi
 
     def city_blocks(self):
         pass
