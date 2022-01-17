4f828ba108c11ea2169df87febe7ca04e77179c4
siavash-saki <siavash.saki@engineer.com>
2022-01-17 19:28:05 +0100

    create POIdata class to send overpass queries

--- tesspy/poi_data.py
+++ tesspy/poi_data.py
@@ -1,57 +1,84 @@
 import numpy as np
 import pandas as pd
-import geopandas as gpd
+from progressbar import progressbar
 import overpass
 import warnings
 
-def create_overpass_query_poi_data(area, poi_categories=['amenity']):
-    """
 
-    :param poi_categories: list
-    :param area: GeoDataFrame
-    :return: GeoDataFrame
-    """
-    # make the query area a bit larger
-    with warnings.catch_warnings():
-        warnings.simplefilter('ignore')
-        area = area.buffer(0.03).simplify(0.01)
+class POIdata:
 
-    # make polygon string for OSM overpass query
-    xy = np.array(area.iloc[0].exterior.coords)
-    poly_str = ''
-    for lat, lon in zip(xy[:, 1], xy[:, 0]):
-        poly_str = poly_str + str(lat) + ' ' + str(lon) + ' '
-    poly_str = poly_str.strip()
+    def __init__(self, area, poi_categories, timeout, verbose):
+        self.area = area
+        self.poi_categories = poi_categories
+        self.timeout = timeout
+        self.verbose = verbose
 
-    # create query string for overpass
-    query_string = ''
-    for element in ['node', 'way']:
-        for poi_category in poi_categories:
-            query_string = query_string + f'{element}[{poi_category}](poly:"{poly_str}");'
-    query_string = "[out:json];(" + query_string + ');out geom;'
+    def create_overpass_query_string(self):
+        # make the query area a bit larger
+        with warnings.catch_warnings():
+            warnings.simplefilter('ignore')
+            area = self.area.buffer(0.03).simplify(0.01)
 
-    return query_string
+        # make polygon string for OSM overpass query
+        xy = np.array(area.iloc[0].exterior.coords)
+        poly_str = ''
+        for lat, lon in zip(xy[:, 1], xy[:, 0]):
+            poly_str = poly_str + str(lat) + ' ' + str(lon) + ' '
+        poly_str = poly_str.strip()
 
+        # create query string for overpass
+        query_string = ''
+        for element in ['node', 'way']:
+            for poi_category in self.poi_categories:
+                query_string = query_string + f'{element}[{poi_category}](poly:"{poly_str}");'
+        query_string = "[out:json];(" + query_string + ');out geom;'
 
+        return query_string
 
-def get_poi_data(query_string, timeout):
+    def get_poi_data(self):
 
-    api = overpass.API(timeout=timeout)
-    resp = api._get_from_overpass(query).json()
+        query_string = self.create_overpass_query_string()
 
+        api = overpass.API(timeout=self.timeout)
+        resp = api._get_from_overpass(query_string).json()
 
+        lst_nodes = []
+        lst_ways = []
 
+        if self.verbose:
+            generator = progressbar(resp['elements'])
+        else:
+            generator = resp['elements']
 
+        for item in generator:
 
+            for cat in self.poi_categories:
+                if cat in item['tags'].keys():
+                    item[cat] = True
+            if item['type'] == 'node':
+                lst_nodes.append(item)
+            elif item['type'] == 'way':
+                item['center_latitude'] = np.mean([point['lat'] for point in item['geometry']])
+                item['center_longitude'] = np.mean([point['lon'] for point in item['geometry']])
+                lst_ways.append(item)
+            else:
+                continue
 
-    pass
+        nodes_df = pd.DataFrame(lst_nodes)
+        ways_df = pd.DataFrame(lst_ways)
 
+        nodes_df['geometry'] = nodes_df[['lon', 'lat']].apply(lambda p: [{'lat': p['lat'], 'lon': p['lon']}], axis=1)
+        nodes_df = nodes_df.rename(columns={'lat': 'center_latitude', 'lon': 'center_longitude'})
+        nodes_df = nodes_df.drop(columns=['id'])
+        ways_df = ways_df.drop(columns=['id', 'bounds', 'nodes'])
 
+        poi_df = pd.concat([ways_df, nodes_df]).fillna(False)
 
+        first_cols = ['type', 'geometry', 'tags', 'center_latitude', 'center_longitude']
+        second_cols = sorted(poi_df.columns.drop(first_cols))
+        poi_df = poi_df[first_cols + second_cols]
 
-
-
-
+        return poi_df
 
 
 def get_road_network(area):
