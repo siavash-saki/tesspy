0fc3144f43297d27be6c749e19b2ca1d3a9ed9dc
siavash-saki <siavash.saki@engineer.com>
2022-02-04 17:45:14 +0100

    Add path to sys path to resolve relative import issue

--- tesspy/__init__.py
+++ tesspy/__init__.py
@@ -1,4 +1,8 @@
-from .tessellation import Tessellation
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+
+from tessellation import Tessellation
 
 name = "tesspy"
 __version__ = "0.0.1"
