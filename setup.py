40a8797e7591e0e67e10de1ce9daf872eb3b31d6
siavash-saki <siavash.saki@engineer.com>
2022-03-07 13:17:18 +0100

    Fixed encoding problem by reading readme

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import setuptools
 
-with open("README.md", "r") as fh:
+with open("README.md", "r", encoding="utf8") as fh:
     LONG_DESCRIPTION = fh.read()
 
 # Packages that tesspy uses explicitly:
